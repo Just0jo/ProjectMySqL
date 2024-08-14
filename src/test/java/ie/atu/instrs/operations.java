68f4a6caf28522db38898e619f6d52213c804629
Just0jo <praiseojofeitimi14@gmail.com>
2024-08-14 11:46:11 +0100

    OPERATIONS

---
+++ src/test/java/ie/atu/instrs/operations.java
@@ -0,0 +1,13 @@
+package ie.atu.instrs;
+
+import java.util.ArrayList;
+
+public interface operations<X> {
+    void add(X item);
+    X get(int id);
+    ArrayList<X> getAll();
+    void update(int id, X item);
+    void delete(int id);
+    //FOR NOW X IS JUST A HOLDER FOR ONE OF THE CALLS I WANT TO PERFORM AN OPERATION ON
+}
+
