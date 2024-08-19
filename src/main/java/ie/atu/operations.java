d9fa6452b5364fe34ef4df25eca82cc07c52aa6b
Just0jo <praiseojofeitimi14@gmail.com>
2024-08-19 09:52:34 +0100

    add instruments into the x spot to show what we are adding updating shoing anf removing

--- src/main/java/ie/atu/operations.java
+++ src/main/java/ie/atu/operations.java
@@ -2,11 +2,11 @@ package ie.atu;
 
 import java.util.ArrayList;
 
-public interface operations<X> {
-    void add(X item);
-    X get(int id);
-    ArrayList<X> getAll();
-    void update(int id, X item);
+public interface operations<instruments> {
+    void add(instruments item);
+    instruments get(int id);
+    ArrayList<instruments> getAll();
+    void update(int id, instruments item);
     void delete(int id);
     //FOR NOW X IS JUST A HOLDER FOR ONE OF THE CALLS I WANT TO PERFORM AN OPERATION ON
 }
